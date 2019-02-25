64ba33516073263ee9cb278819d678980edf13e2
Joel Bartlett <joel@murbar.com>
2019-02-25 17:08:40 -0500

    Add click handler

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -33,10 +33,10 @@ export default class Movie extends Component {
   //   }
   // }
 
-  // saveMovie = () => {
-  //   const addToSavedList = this.props.addToSavedList;
-  //   addToSavedList(this.state.movie)
-  // }
+  saveMovie = () => {
+    const addToSavedList = this.props.addToSavedList;
+    addToSavedList(this.state.movie);
+  };
 
   render() {
     if (!this.state.movie) {
@@ -46,7 +46,9 @@ export default class Movie extends Component {
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
-        <div className="save-button">Save</div>
+        <div className="save-button" onClick={this.saveMovie}>
+          Save
+        </div>
       </div>
     );
   }
