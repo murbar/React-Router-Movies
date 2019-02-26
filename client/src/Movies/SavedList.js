f6eb971d0b927a7ed04814514892ae5ac4a699cd
Joel Bartlett <joel@murbar.com>
2019-02-26 15:57:16 -0500

    Add key props

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -11,7 +11,7 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <NavLink to={`/movies/${movie.id}`}>
+          <NavLink to={`/movies/${movie.id}`} key={movie.id}>
             <span className="saved-movie">{movie.title}</span>
           </NavLink>
         ))}
